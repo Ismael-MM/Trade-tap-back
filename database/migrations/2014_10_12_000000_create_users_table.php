004d66784e08dd1e5e9acfca54a4470ae5b7389a
ismael <ismontelongo@outlook.com>
2024-01-31 14:15:08 +0000

    add: breeze y users = cliente

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('apellido1');
+            $table->string('apellido2');
+            $table->string('direccion');
+            $table->string('provincia');
+            $table->string('localidad');
+            $table->integer('CP');
+            $table->string('telefono')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');

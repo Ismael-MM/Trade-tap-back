cd38eeec842da4d68b47c76a76793a04fb1026fc
ismael <ismontelongo@outlook.com>
2024-02-05 17:36:16 +0000

    cambios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->morphs('userable');
+            $table->nullableMorphs('userable');
             $table->timestamps();
         });
     }

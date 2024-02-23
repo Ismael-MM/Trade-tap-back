357c14e014e4f4587958f79962cf820994ef1bea
ismael <ismontelongo@outlook.com>
2024-02-23 20:23:35 +0000

    add: configuracion cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,26 @@ return [
     |
     */
 
-    'paths' => ['*'],
+
+    'paths' => [
+
+        'api/*',
+
+        'sanctum/csrf-cookie',
+
+        'login',
+
+        'register',
+
+        'logout',
+
+        'forgot-password',
+
+        'reset-password',
+
+        'verify-email',
+
+    ],
 
     'allowed_methods' => ['*'],
 

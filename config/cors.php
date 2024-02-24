256b34a63790fe4291fe575c5be4c2a5b5f8d7ca
ismael <ismontelongo@outlook.com>
2024-02-24 15:04:57 +0000

    test cors algun numero

--- config/cors.php
+++ config/cors.php
@@ -16,25 +16,7 @@ return [
     */
 
 
-    'paths' => [
-
-        'api/*',
-
-        'sanctum/csrf-cookie',
-
-        'login',
-
-        'register',
-
-        'logout',
-
-        'forgot-password',
-
-        'reset-password',
-
-        'verify-email',
-
-    ],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
